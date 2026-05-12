646e180cfb2a5a881c41c377e0f6df4b43b7b002
ystc1247 <ystc1247@khu.ac.kr>
2026-05-12 15:43:45 +0900

    chore: scaffold spring boot backend

---
+++ src/test/kotlin/com/blockchain2026/team4/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blockchain2026.team4.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
