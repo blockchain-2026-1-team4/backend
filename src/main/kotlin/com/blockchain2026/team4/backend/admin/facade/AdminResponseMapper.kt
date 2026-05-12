074c49ec74317e92230a8c0468caa8855aa3688d
ystc1247 <ystc1247@khu.ac.kr>
2026-05-12 17:49:19 +0900

    admin: map resale transaction responses

--- src/main/kotlin/com/blockchain2026/team4/backend/admin/facade/AdminResponseMapper.kt
+++ src/main/kotlin/com/blockchain2026/team4/backend/admin/facade/AdminResponseMapper.kt
@@ -1,12 +1,20 @@
 package com.blockchain2026.team4.backend.admin.facade
 
 import com.blockchain2026.team4.backend.admin.controller.response.BlockchainTransactionResponse
+import com.blockchain2026.team4.backend.admin.controller.response.ResaleTransactionResponse
 import com.blockchain2026.team4.backend.blockchain.dto.BlockchainTransactionDto
+import com.blockchain2026.team4.backend.resale.dto.ResaleListingDto
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper
 interface AdminResponseMapper {
     fun toBlockchainTransactionResponse(dto: BlockchainTransactionDto): BlockchainTransactionResponse
 
     fun toBlockchainTransactionResponses(dtos: List<BlockchainTransactionDto>): List<BlockchainTransactionResponse>
+
+    @Mapping(source = "id", target = "listingId")
+    fun toResaleTransactionResponse(dto: ResaleListingDto): ResaleTransactionResponse
+
+    fun toResaleTransactionResponses(dtos: List<ResaleListingDto>): List<ResaleTransactionResponse>
 }
