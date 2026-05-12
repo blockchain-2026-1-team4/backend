3083ee9f66f954181de8e63f29044f36ffa726b4
ystc1247 <ystc1247@khu.ac.kr>
2026-05-12 16:01:29 +0900

    build: add backend dependencies and jvm settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
+	kotlin("kapt") version "2.2.21"
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "2.2.21"
@@ -12,13 +15,19 @@ description = "Blockchain ticket backend"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(26)
 	}
 }
 
 repositories {
 	mavenCentral()
 }
+
+val jjwtVersion = "0.13.0"
+val mapstructVersion = "1.6.3"
+val springdocVersion = "3.0.3"
+val web3jVersion = "5.0.2"
+val zxingVersion = "3.5.4"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -30,7 +39,16 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
+	implementation("org.mapstruct:mapstruct:$mapstructVersion")
+	implementation("org.web3j:core:$web3jVersion")
+	implementation("com.google.zxing:core:$zxingVersion")
+	implementation("com.google.zxing:javase:$zxingVersion")
+	implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
 	runtimeOnly("org.postgresql:postgresql")
+	kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-liquibase-test")
@@ -44,7 +62,19 @@ dependencies {
 
 kotlin {
 	compilerOptions {
+		jvmTarget.set(JvmTarget.JVM_24)
 		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<JavaCompile> {
+	options.release.set(24)
+}
+
+kapt {
+	arguments {
+		arg("mapstruct.defaultComponentModel", "spring")
+		arg("mapstruct.unmappedTargetPolicy", "ERROR")
 	}
 }
 
