536e05039d69a3b057232c41ee8d3af027fc103e
ystc1247 <ystc1247@khu.ac.kr>
2026-05-12 16:23:19 +0900

    test: add h2 runtime for context tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
