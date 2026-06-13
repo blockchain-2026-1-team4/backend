3ad8705faffd843a77f2eef8d468090ee16c7dd3
ystc1247 <ystc1247@khu.ac.kr>
2026-06-13 16:17:28 +0900

    chore: update blockchain property defaults

--- src/main/kotlin/com/blockchain2026/team4/backend/common/config/AppProperties.kt
+++ src/main/kotlin/com/blockchain2026/team4/backend/common/config/AppProperties.kt
@@ -56,9 +56,9 @@ data class AppProperties(
         val networkName: String = "kaia-kairos-testnet",
         val rpcUrl: String = "https://public-en-kairos.node.kaia.io",
         val chainId: Long = 1001,
-        val contractAddress: String = "0xce763CEefFA79695a2A2499a80ff40A27a4678f5",
+        val contractAddress: String = "0x790aa2356BAb711998faA9c58dCDD47205e6683d",
         val operatorPrivateKey: String = "",
-        val gasPriceWei: BigInteger = BigInteger.valueOf(1_000_000_000L),
+        val gasPriceWei: BigInteger = BigInteger.valueOf(30_000_000_000L),
         val gasLimit: BigInteger = BigInteger.valueOf(6_500_000L),
     )
 }
